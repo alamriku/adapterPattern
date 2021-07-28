3b859475321fbd958280178cb76ebfdc69a5f9a0
Riku <alamriku069@gmail.com>
2021-07-28 11:45:42 +0600

    comment about composition class added

--- app/Adapter/ScoreIntegrationAdapter.php
+++ app/Adapter/ScoreIntegrationAdapter.php
@@ -17,10 +17,15 @@ class ScoreIntegrationAdapter implements ScoreIntegrationTarget
     }
 
 
+//Composition is a relationship between classes that allows one class to contain another.
+//Composition should typically be favored above inheritance, as it’s more flexible.
+//Composition allows for a more loosely coupled structure that avoids a fragile hierarchy of classes.
+//Composition is a Has-A relationship. A car has a steering wheel.
 //we use composition to wrap the old integration (object),
 //which is basically hidden behind the ScoreIntegrationTarget interface.
 //If you think about the class that will use it, the only thing it will know is the Interface.
 //Inside the method, “getUserScore” is where the real adaptation of interfaces takes place.
+//Adapter has a adaptee. The adaptee is  oldIntegration->getUserSore()
     public function getUserScore(string $uuid): UserScore
     {
         $rawUser = $this->oldIntegration->getUserScore($uuid);
